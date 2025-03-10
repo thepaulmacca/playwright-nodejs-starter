c782600fb242b9edcd0af3f84b6c269fcf938070
Paul McDonald <me@thepaulmacca.com>
2025-03-10 14:13:06 +0000

    Add tests

---
+++ tests/m6-advanced-actions/_1dialogs.test.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test";
+
+const name = "Sofia";
+
+test('Dialog test - Default handling is to dismiss', async ({ page }) => {
+
+  await page.goto('/');
+
+  const input = page.getByLabel('First name');
+  await input.fill(name);
+
+  await expect(input).toHaveValue(name);
+
+  await page.getByRole('button', { name: 'Clear' }).click();
+  await expect(input).toHaveValue(name);
+
+});
+
+test('Dialog test - OK or dismiss', async ({ page }) => {
+
+  page.on('dialog', dialog => dialog.accept());
+
+  await page.goto('/');
+
+  const input = page.getByLabel('First name');
+  await input.fill(name);
+
+  await expect(input).toHaveValue(name);
+
+  await page.getByRole('button', { name: 'Clear' }).click();
+  await expect(input).toHaveValue('');
+
+});
